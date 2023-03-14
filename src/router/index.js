204667e5a39396f18589b5ff01b327da9d357fac
Dofir Fauzi <dofirfauzi@tazkia.ac.id>
2023-03-14 13:41:28 +0700

    add synamic meta title

--- src/router/index.js
+++ src/router/index.js
@@ -15,48 +15,83 @@ const routes = [
   {
     path: "/",
     component: Login,
+    meta:{
+      title : 'Login',
+    }
   },
   {
     path: "/total-dana-asr",
     component: TotalDanaAsr,
+    meta:{
+      title : 'Total Dana Asr',
+    }
   },
   {
     path: "/wilayah-kerja",
     component: WilayahKerja,
+    meta:{
+      title : 'Wilayah Kerja',
+    }
   },
   {
     path: "/rekening-dan-saldo",
     component: RekeningDanSaldo,
+    meta:{
+      title : 'Rekening Dan Saldo',
+    }
   },
   {
     path: "/daftar-transaksi",
     component: DaftarTransaksi,
+    meta:{
+      title : 'Daftar Transaksi',
+    }
   },
   {
     path: "/info-suku-bunga",
     component: InfoSukuBunga,
+    meta:{
+      title : 'Info Suku Bunga',
+    }
   },
   {
     path: "/mitra",
     component: Mitra,
+    meta:{
+      title : 'Mitra',
+    }
   },
   {
     path: "/users",
     component: Users,
+    meta:{
+      title : 'Users',
+    }
   },
   {
     path: "/my-profile",
     component: MyProfile,
+    meta:{
+      title : 'My Profile',
+    }
   },
   {
     path: "/reset-password",
     component: ResetPassword,
+    meta:{
+      title : 'Reset Password',
+    }
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to ,from ,next) => {
+  document.title = `${to.meta.title}`;
+  next();
 });
 
 export default router;
